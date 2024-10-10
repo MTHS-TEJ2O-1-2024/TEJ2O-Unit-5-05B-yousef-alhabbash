ef557d140bcae5a95efacf94a59705cf1a2de18f
yousef-alhabbash <yousef.al-habbash@mths.ca>
2024-10-10 10:13:38 -0400

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,44 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Mr. Yousef
+ * Created on: Oct 2024
+ * This program trafic lights
 */
 
-basic.showString('Hello, World!')
+
+let neopixelStrip: neopixel.Strip = null
+
+
+basic.clearScreen()
+neopixelStrip = neopixel.create(DigitalPin.P16, 3, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))  // Green
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))  // Yellow
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))  // Red
+neopixelStrip.show()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    // Green 
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))  
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))  
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))  
+    neopixelStrip.show()
+    basic.pause(1000)
+
+    // Yellow 
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))  
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow)) 
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))  
+    neopixelStrip.show()
+    basic.pause(1000)
+
+     // Red    
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))  
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))  
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))    
+    neopixelStrip.show()
+    basic.pause(1000)
+
+    
+    basic.showIcon(IconNames.Happy)
+})
